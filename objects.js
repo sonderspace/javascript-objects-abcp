866678857e5b8254e837e26f6ba6a48f44250361
Efrain Lopez <lopezefrain5894@gmail.com>
2018-01-22 20:11:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,9 @@ let playlist = {
   artist: 'norma',
   song: 'she sings',
 }
+
+function updatePlaylist(playlist, artistName, songTitle)
+{
+  // the key-value is assigned destructively
+  playlist[artistName] = songTitle;
+}
