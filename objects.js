e93a682b5896500d510c8cad8d7812714ae6ec4f
Efrain Lopez <lopezefrain5894@gmail.com>
2018-01-22 20:25:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -11,5 +11,5 @@ function updatePlaylist(playlist, artistName, songTitle)
 function removeFromPlaylist(playlist, artistName)
 {
   // this procedure removes a key-value from an Object
-  
+  delete playlist.artistName;
 }
